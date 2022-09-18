ed0fe8b624cd0e7cc6976bee9f97f6f7802a1a57
pawel_stepniak <pawelstepniak22@gmail.com>
2022-09-19 00:32:05 +0200

    begin footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,49 @@
 export default function footer() {
   const footer = document.createElement("footer");
-  
+  footer.classList.add("footer");
+
+  footer.appendChild(
+    createLink(
+      "mailto:pawelstepniak22@gmail.com",
+      "pawelstepniak22@gmail.com",
+      "footer__link"
+    )
+  );
+
+  const unorderdList = document.createElement("ul");
+  unorderdList.classList.add("social-list");
+  footer.appendChild(unorderdList);
+
+  const socialListTwitter = document.createElement("li");
+  socialListTwitter.classList.add("social-list__item");
+  unorderdList.appendChild(socialListTwitter);
+
+  socialListTwitter.appendChild(
+    createLink(
+      "https://twitter.com/kainpets",
+      "placeholder",
+      "social-list__link"
+    )
+  );
+
+  const twitterIcon = document.createElement("i");
+  twitterIcon.classList.add("fab");
+  twitterIcon.classList.add("fa-twitter");
+  socialListTwitter.appendChild(twitterIcon);
+
+  const socialListGithub = document.createElement("li");
+  socialListGithub.classList.add("social-list__item");
+  unorderdList.appendChild(socialListGithub);
 
   return footer;
 }
 
 document.body.appendChild(footer());
+
+function createLink(url, text, className) {
+  const link = document.createElement("a");
+  link.href = url;
+  link.textContent = text;
+  link.classList.add(className);
+  return link;
+}
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./style.css";
 import "./components/header";
 import "./components/getData";
 import "./components/mainComponent";
+import "./components/footer";

ad53d281ab706458f319b7cf67e2769ce8f4632e
kainpets <pawelstepniak22@gmail.com>
2022-09-28 19:10:07 +0200

    add additional table cell

--- src/components/tableComponent.js
+++ src/components/tableComponent.js
@@ -7,19 +7,19 @@ export default function createTable() {
   thead.appendChild(tr);
 
   const th0 = createTh();
-  th0.textContent = "";
+  th0.textContent = "wysokość roszczenia";
   tr.appendChild(th0);
 
   const th1 = createTh();
-  th1.textContent = "> 5000 zł";
+  th1.textContent = "< 5000 zł";
   tr.appendChild(th1);
 
   const th2 = createTh();
-  th2.textContent = "> 50.000 zł";
+  th2.textContent = "< 50.000 zł";
   tr.appendChild(th2);
 
   const th3 = createTh();
-  th3.textContent = "< 50.000 zł";
+  th3.textContent = "> 50.000 zł";
   tr.appendChild(th3);
 
   const tbody = document.createElement("tbody");

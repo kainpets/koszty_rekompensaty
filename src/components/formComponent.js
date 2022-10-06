185bd13bb090d5a8e4f188b943b81b737210836f
kainpets <pawelstepniak22@gmail.com>
2022-10-06 21:08:34 +0200

    add maximum date validation up to current day

--- src/components/formComponent.js
+++ src/components/formComponent.js
@@ -8,11 +8,14 @@ export default function form() {
   form.id = "form";
   form.classList.add("main");
 
+  const currentDate = new Date().toJSON().slice(0, 10);
+
   const userDateInput = document.createElement("input");
   userDateInput.id = "year";
   userDateInput.setAttribute("type", "date");
   userDateInput.setAttribute("name", "date");
   userDateInput.setAttribute("min", "2002-02-01");
+  userDateInput.setAttribute("max", `${currentDate}`);
   form.appendChild(userDateInput);
 
   return form;
